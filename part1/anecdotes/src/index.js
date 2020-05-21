2bce7395d3956d5f49126535773db8abd9744fbb
Sara Pentti <saraarmiida@Sara-MacBook-Pro.local>
2020-05-21 15:48:04 +0300

    anecdotes step 3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -7,6 +7,20 @@ const Button = (props) => (
   </button>
 )
 
+function mostVotes(votes) {
+  var max = votes[0];
+  var maxIndex = 0;
+
+  for (var i = 1; i < votes.length; i++) {
+      if (votes[i] > max) {
+          maxIndex = i;
+          max = votes[i];
+      }
+  }
+
+  return maxIndex;
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(Math.floor((Math.random()*anecdotes.length)))
   const [votes, setVotes] = useState(Array.apply(null, new Array(6)).map(Number.prototype.valueOf,0))
@@ -16,14 +30,21 @@ const App = (props) => {
     newVotes[selected] += 1
     setVotes(newVotes)
   }
-  console.log(votes)
 
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-      <Button handleClick={handleVoteClick} text="vote"/>
-      <Button handleClick={() => setSelected(Math.floor((Math.random()*anecdotes.length)))} text='next anecdote'/>
+      <div>
+        <h2>Anecdote of the day</h2>
+        {props.anecdotes[selected]}<br/>
+        has {votes[selected]} votes<br/>
+        <Button handleClick={handleVoteClick} text="vote"/>
+        <Button handleClick={() => setSelected(Math.floor((Math.random()*anecdotes.length)))} text='next anecdote'/>
+      </div>
+      <div>
+        <h2>Anecdote with most votes</h2>
+        {props.anecdotes[mostVotes(votes)]}<br/>
+        has {votes[mostVotes(votes)]} votes<br/>
+      </div>
     </div>
   )
 }
