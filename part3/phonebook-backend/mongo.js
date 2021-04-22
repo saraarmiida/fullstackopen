c7f33e019d793227e4213fff3e446f80a58e1e74
saraarmiida <sara.pentti@hotmail.fi>
2021-04-22 12:40:09 +0300

    3.12 command line database

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (!(process.argv.length === 3 || process.argv.length === 5)) {
+  console.log('Usage:\n	display all entries:	node mongo.js <password>\n	add new number:		node mongo.js <password> <name> <number>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+`mongodb+srv://test:${password}@cluster0.jlg8p.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+	Person
+	.find({})
+		.then(result => {
+			console.log('phonebook:')
+			result.forEach(contact => {
+				console.log(contact.name, contact.number)
+			})
+			mongoose.connection.close()
+		})
+} else {
+	const name = process.argv[3]
+	const number = process.argv[4]
+
+	const person = new Person({
+	  name: name,
+	  number: number
+	})
+	
+	person.save().then(result => {
+	  console.log('added', name, 'number', number, 'to phonebook')
+	  mongoose.connection.close()
+	})
+}
