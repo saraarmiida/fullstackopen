ed7006564815dcb5a74fa0172b9f2c54062b014e
saraarmiida <sara.pentti@hotmail.fi>
2021-04-25 20:50:40 +0300

    3.15-3.18 phonebook database steps 3 to 6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -7,21 +7,29 @@ const Person = require('./models/person')
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(cors())
+app.use(express.json())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.use(express.json())
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
 
-app.use(express.static('build'))
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+
+	next(error)
+}
 
 app.get('/info', (request, response) => {
-	const phonebook = `Phonebook has info for ${persons.length} people`
+	const phonebook = `Phonebook has info for ${Person.length} people`
 	const date = Date()
 	const info = phonebook + '<br><br>' + date
-
+	
 	response.send(info)
 })
 
@@ -31,37 +39,61 @@ app.get('/api/persons', (request, response) => {
 	})
 })
 
-app.get('/api/persons/:id', (request, response) => {
-	Person.findById(request.params.id).then(person => {
-		response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+	Person.findById(request.params.id)
+	.then(person => {
+		if (person) {
+			response.json(person)
+		} else {
+			response.status(404).end()
+		}
 	})
+	.catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter(person => person.id !== id)
-
-	response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+	Person.findByIdAndRemove(request.params.id)
+	.then(result => {
+		response.status(204).end()
+	})
+	.catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
 	const body = request.body
-
+	
 	if (!body.name || !body.number) {
 		return response.status(400).json({
 			error: 'the name or number is missing'
 		})
 	}
-
+	
 	const person = new Person({
 		name: body.name,
 		number: body.number,
 	})
-
+	
 	person.save().then(savedContact => {
 		response.json(savedContact)
 	})
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+	const body = request.body
+
+	const person = {
+		name: body.name,
+		number: body.number,
+	}
+
+	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+		.then(updatedPerson => {
+			response.json(updatedPerson)
+		})
+		.catch(error => next(error))
+})
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
